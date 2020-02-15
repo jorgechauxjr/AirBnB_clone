2720ac38be66ce29f3146092ab206798b9021528
jorgechauxjr <chauxjorge@hotmail.com>
2020-02-15 17:12:46 +0000

    Add first version 7.Console0.1

--- console.py
+++ console.py
@@ -15,6 +15,28 @@ class HBNBCommand(cmd.Cmd):
         """Command that quit"""
         return True
 
+    def do_create(self, line):
+        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id"""
+        pass
+
+    def do_show(self, line):
+        """Prints the string representation of an instance based on the class name and id"""
+        pass
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+        pass
+
+    def do_all(self, line):
+        """ Prints all string representation of all instances based or not on the class name."""
+        pass
+
+    def do_update(self, line):
+        """Updates an instance based on the class name and id by adding or updating attribute """
+        pass
+    
+
+
     def emptyline(self):
         """print a new empty line"""
         if self.lastcmd:
