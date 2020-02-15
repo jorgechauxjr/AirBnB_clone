1b0c312ad79f19100ac342e91bac3c4083b792cc
jorgechauxjr <chauxjorge@hotmail.com>
2020-02-15 19:29:13 +0000

    Add attributes to State, City and Amenity classes TsK 8.More classes!

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Amenity class """
+
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """class Amenity that inherits from BaseModel"""
+
+    name = ""
