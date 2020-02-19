d6a88da1e22949c4ce8508777954e29ab77b2e9d
alejolo311 <alejolo311@gmail.com>
2020-02-19 12:58:52 +0000

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """str format"""
@@ -37,7 +37,7 @@ class BaseModel:
     def save(self):
         """updates."""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """dictionary format"""
